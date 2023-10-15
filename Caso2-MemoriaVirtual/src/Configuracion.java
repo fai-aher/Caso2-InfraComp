cdbed720e9fc93591008e629dedeca91596912ab
Alejandr2106 <a.gonzalez48@uniandes.edu.co>
2023-10-14 23:31:10 -0500

    Generar archivo
    Generar archivo

--- Caso2-MemoriaVirtual/src/Configuracion.java
+++ Caso2-MemoriaVirtual/src/Configuracion.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Configuracion {
@@ -35,6 +38,7 @@ public class Configuracion {
 
 			if(modo == 1) {
 				//Aqui va el metodo del modo 1
+                modo1();
 			}
 			else if(modo == 2) {
 				//Aqui va el metodo del modo 2
@@ -49,6 +53,7 @@ public class Configuracion {
     }
 
     public void modo1(){
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Digite el tamano de pagina:");
@@ -60,10 +65,39 @@ public class Configuracion {
         System.out.println("Digite el numero deseado de columnas para las matrices:");
 		numeroColumnas = sc.nextInt();
 
+        try {
+            int TP = tamPagina;
+            int NF = numeroFilas;
+            int NC1 = numeroColumnas;
+            int NC2 = numeroColumnas;
+            int NR = calcularNumeroDeReferencias(TP, NF, NC1, NC2);
+            int NP = calcularNumeroDePaginas(TP, NF, NC1, NC2);
+
+            // Aquí se crea un archivo 
+            BufferedWriter writer = new BufferedWriter(new FileWriter("referencias.txt"));
+            writer.write("TP:" + TP + ", NF:" + NF + ", NC1:" + NC1 + ", NC2:" + NC2 + ", NR:" + NR + ", NP:" + NP);
+            writer.newLine();
+
+            writer.close();
+
+            System.out.println("Referencias generadas y almacenadas en el archivo referencias.txt.");
+        } catch (IOException e) {
+            System.out.println("Error al escribir el archivo de referencias: " + e.getMessage());
+        }
     }
 
     public void modo2(){
 
+    }
+
+
+
+    public int calcularNumeroDeReferencias(int tamPagina, int NF, int NC1, int NC2){
+        return 0;
+    }
+
+    public int calcularNumeroDePaginas(int tamPagina, int NF, int NC1, int NC2){
+        return 0; 
     }
 
 
